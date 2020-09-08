d8dbcb3dbbff1331b75d191ea784d9b9081d2a95
barackmaund1 <pmaundu49@gmail.co>
2020-09-08 04:32:32 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZZkVufUoQIdaBVVxydKn310TFJPbxNbs",
+    authDomain: "whatsapp-clone-ed53e.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-ed53e.firebaseio.com",
+    projectId: "whatsapp-clone-ed53e",
+    storageBucket: "whatsapp-clone-ed53e.appspot.com",
+    messagingSenderId: "265333758564",
+    appId: "1:265333758564:web:c9d48de2a819e6e9669d71",
+    measurementId: "G-ZGQB4Y2KVY"
+};
